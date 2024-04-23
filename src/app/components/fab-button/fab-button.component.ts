9a2f5f8f11cfb38b3c446d7bbbae3ddd07a7e99b
ekrenz <ekrenz-developer@gmail.com>
2024-04-23 05:42:34 +0100

    add animation

--- src/app/components/fab-button/fab-button.component.ts
+++ src/app/components/fab-button/fab-button.component.ts
@@ -1,12 +1,28 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { trigger, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-fab-button',
   standalone: true,
   imports: [],
   templateUrl: './fab-button.component.html',
+  animations: [
+    trigger('scaleIn', [
+      transition(':enter', [
+        style({
+          transform: 'translate(-50%, -50%) scale(0)',
+          opacity: 0
+        }),
+        animate('500ms ease-in-out', style({
+          transform: 'translate(0, 0) scale(1)',
+          opacity: 1
+        }))
+      ])
+    ])
+  ]
 })
 export class FabButtonComponent {
+  @Input() loading = true;
   @Output() toggleClick = new EventEmitter<void>();
 
   onClick() {
